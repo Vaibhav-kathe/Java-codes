d2f900c6b8ab6b9d8fd62759cb5ea91105f9df4c
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-05-10 23:25:48 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,10 +1,27 @@
 import java.util.*;
-public class prime {
-    public static void main(String args[])
-    {
+public class Prime {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
         int number = sc.nextInt();
-        
+
+        if (number <= 1) {
+            System.out.println(number + " is not a prime number.");
+        } else {
+            int count = 0;
+
+            for (int i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    count++;
+                    break; 
+                }
+            }
+
+            if (count == 0)
+                System.out.println(number + " is a prime number.");
+            else
+                System.out.println(number + " is not a prime number.");
+        }
+        sc.close();
     }
-    
 }
