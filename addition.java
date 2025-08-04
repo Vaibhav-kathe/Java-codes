7e65a25f9fb8530aa00e86b55755b2b9d7ebcce5
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-04 17:53:44 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class addition {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         //System.out.println(a);
@@ -10,6 +9,6 @@ public class addition {
         int sum = a + b ;
         System.out.println("sum = " + sum);
         sc.close();
-    
     }
 }
+
