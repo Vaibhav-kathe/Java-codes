0dce12f0a2892d9145b4509d20404e1043e5b43b
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-12 18:55:13 +0530

    Create 07.floatNumber.java

---
+++ 07.floatNumber.java
@@ -0,0 +1,16 @@
+package challenges;
+import java.util.Scanner;
+public class FloatNumber {
+	public static void main(String[] args) {
+		System.out.print("Welcome to Product of Floating Number\n");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the value of A = ");
+		float A = sc.nextFloat();
+		
+		System.out.print("Enter the value of B = ");
+		float B = sc.nextFloat();
+		
+		System.out.println("Product is : "+(A*B));
+	}
+}
