0349a6cbccfaa32d4c5de006f45b98e34b123fd6
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-12 19:47:10 +0530

    Create 09.areaOfTriangle.java

---
+++ 09.areaOfTriangle.java
@@ -0,0 +1,18 @@
+package challenges;
+import java.util.*;
+public class AreaOfTriangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Area of Triangle\n");
+		
+		System.out.print("Enter the Base(cm) = ");
+		double B = sc.nextDouble();
+
+		System.out.print("Enter the Height(cm) = ");
+		double H = sc.nextDouble();
+		
+		double Area = 0.5*B*H;
+		System.out.print("Area of Triangle : "+Area);
+	}
+
+}
