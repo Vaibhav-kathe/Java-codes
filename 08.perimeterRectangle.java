9599ab8840dcfebe156bdd19f61170c6a6c70315
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-12 19:32:40 +0530

    Create 08.perimeterRectangle.java

---
+++ 08.perimeterRectangle.java
@@ -0,0 +1,16 @@
+package challenges;
+import java.util.*;
+public class PerimeterRectangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Perimeter of Rectangle\n");
+		System.out.print("Enter all 4 slide(cm) = ");
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		
+		int Perimeter = A+B+C+D;
+		System.out.println("Perimeter of Rectangle is  : "+Perimeter+" cm ");
+	}
+}
