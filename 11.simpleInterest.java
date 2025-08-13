4fa2921453c2e95d8876d3d20e66914f5d0ae228
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-13 07:41:01 +0530

    Create 11.simpleInterest.java

---
+++ 11.simpleInterest.java
@@ -0,0 +1,20 @@
+package challenges;
+import java.util.Scanner;
+public class SimpleInterest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Simple Interest.....!\n");
+		
+		System.out.print("Enter the Principle($)= ");
+		int P = sc.nextInt();
+		
+		System.out.print("Enter the Time(Yrs)= ");
+		int T = sc.nextInt();
+		
+		System.out.print("Enter the Rate of Interest(%)= ");
+		double R = sc.nextDouble();
+		
+		double SI = (P*T*R)/100;
+		System.out.print("Simple Interest = "+SI+" $ ");
+	}
+}
