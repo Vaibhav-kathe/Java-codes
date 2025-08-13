2ed22b4fbd41679d4adfa3e5ef5f31243dda988c
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-13 07:39:37 +0530

    Create 10.temperatureConvertor.java

---
+++ 10.temperatureConvertor.java
@@ -0,0 +1,14 @@
+package challenges;
+import java.util.Scanner;
+public class TemperatureConverter {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Temperature Converter...\n");
+		
+		System.out.print("\nEnter the temp in Fahrenheit(F)= ");
+		int F = sc.nextInt();
+	
+		int C = (F-32)*5/9;
+		System.out.print("Temperature in Celsius = "+C+" C");
+	}
+}
