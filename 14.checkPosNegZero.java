f646d7ab538acb110365089a737d43c41c4f97e3
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-14 18:58:01 +0530

    Create 14.checkPosNegZero.java

---
+++ 14.checkPosNegZero.java
@@ -0,0 +1,21 @@
+package challenges;
+import java.util.*;
+public class CheckPosNegZero {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Check Number\n");
+		System.out.print("Enter the Number= ");
+		int Number = sc.nextInt();
+		
+		if(Number>0) {
+			System.out.print("Number is positive");
+		}else if(Number<0) {
+			System.out.print("Number is negative");
+		}else if(Number==0) {
+			System.out.print("Number is zero");
+		}else {
+			System.out.print("Invalid Number");
+		}
+		sc.close();
+	}
+}
