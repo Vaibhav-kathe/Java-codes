b471d52030028d5dae2e1168a45e325158b0372c
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-14 19:29:39 +0530

    Create 17.leapYear.java

---
+++ 17.leapYear.java
@@ -0,0 +1,16 @@
+package challenges;
+import java.util.Scanner;
+public class LeapYear {
+	public static void main(String[] args) {
+		System.out.println("Checking Leaf Year....");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the year = ");
+		int year = sc.nextInt();
+		
+		if(year%400==0 || ((year%100 !=0) && (year%4 ==0))) {
+			System.out.print(year+" is LEAF Year");
+		}else {
+			System.out.print(year+" is not a LEAF Year");
+		}
+	}
+}
