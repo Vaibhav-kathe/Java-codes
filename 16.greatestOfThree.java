fa9f65533ef73b3107b0a9a52e48a3cb16b559dc
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-14 19:25:14 +0530

    Update 16.greatestOfThree.java

--- 16.greatestOfThree.java
+++ 16.greatestOfThree.java
@@ -26,3 +26,32 @@ public class GreatestOfThree {
 		sc.close();
 	}
 }
+
+// Advanced method
+import java.util.Scanner;
+public class GreatestOfThree {
+	public static void main(String[] args) {
+		System.out.println("Calculating Greatest Number...");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the first number = ");
+		int first = sc.nextInt();
+		
+		System.out.print("Enter the second number = ");
+		int second = sc.nextInt();
+		
+		System.out.print("Enter the third number = ");
+		int third = sc.nextInt();
+		
+		if(first >= second && first >= third) {
+			System.out.println(first+" is greatest number");
+		}else if(second >= third) {
+			System.out.println(second+" is greatest number");
+		}else{
+			System.out.println(third+" is greatest number");
+		}
+		sc.close();
+	}
+}
+
+
