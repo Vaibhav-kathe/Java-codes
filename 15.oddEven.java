afb5dc2c74d4a4ba7c1a4d3feac7ef8d10e47cf5
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-14 19:00:27 +0530

    Create 15.oddEven.java

---
+++ 15.oddEven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class OddEven {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Check Number\n");
+		System.out.print("Enter the Number = ");
+		int Number = sc.nextInt();
+		
+		if(Number%2==0) {
+			System.out.print("Number is EVEN");
+		}else{
+			System.out.print("Number is ODD");
+		}
+	}
+}
