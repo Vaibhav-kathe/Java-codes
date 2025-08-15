77a3d8bb1ca45321d637abb4e6bba60f28e358dc
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-15 10:04:18 +0530

    Create 21.XORBitwise.java

---
+++ 21.XORBitwise.java
@@ -0,0 +1,17 @@
+package challenges;
+import java.util.Scanner;
+public class XORBitwise {
+	public static void main(String[] args) {
+		System.out.println("Preforming XOR Bitwise Operation...");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the 1st Number= ");
+		int first = sc.nextInt();
+		
+		System.out.print("Enter the 2nd Number= ");
+		int second = sc.nextInt();
+		
+		//output is 0 if bit are 0,0 or 1,1, otherwise 1
+		int result = first ^ second;
+		System.out.print("\nResult is "+result);	
+	}
+}
