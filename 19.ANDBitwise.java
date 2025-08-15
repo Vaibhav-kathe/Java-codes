c40581d95c203282648215d4d5c057aac0912e7b
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-15 10:00:41 +0530

    Create 19.ANDBitwise.java

---
+++ 19.ANDBitwise.java
@@ -0,0 +1,17 @@
+package challenges;
+import java.util.Scanner;
+public class ANDBitwise {
+	public static void main(String[] args) {
+		System.out.println("Preforming AND Bitwise Operation...");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the 1st Number= ");
+		int first = sc.nextInt();
+		
+		System.out.print("Enter the 2nd Number= ");
+		int second = sc.nextInt();
+		
+		//output is 1 if bits of both operand is 1, otherwise 0
+		int result = first & second;
+		System.out.print("\nResult is "+result);	
+	}
+}
