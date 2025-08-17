912ddb11befc58495c4a395845b512ec96c77361
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-17 22:21:20 +0530

    Create 23.leftShiftOperator.java

---
+++ 23.leftShiftOperator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class LeftShiftOperator {
+	public static void main(String[] args) {
+		System.out.println("Preforming Left Shift Operator...");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number= ");
+		int num = sc.nextInt();
+		
+		//filling the new rightmost bit with 0's
+		int result = num << 1;
+		System.out.print("\nResult is "+result);	
+	}
+}
