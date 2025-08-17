406e14bdd1aec74dd08cabcbd071828ce18e9f55
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-17 22:22:55 +0530

    Create 24.rightShiftOperator.java

---
+++ 24.rightShiftOperator.java
@@ -0,0 +1,14 @@
+package challenges;
+import java.util.Scanner;
+public class RightShiftOperator {
+	public static void main(String[] args) {
+		System.out.println("Preforming Right Shift Operator...");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number= ");
+		int num = sc.nextInt();
+		
+		//filling the new leftmost bit with 0's
+		int result = num >> 1;
+		System.out.print("\nResult is "+result);
+	}
+}
