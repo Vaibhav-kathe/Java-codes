78ea6cf43e34a805df8b3faf6d5673cdf45682c1
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-17 22:24:52 +0530

    Create 25.oddEvenBitwise.java

---
+++ 25.oddEvenBitwise.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class OddEvenBitwise {
+	public static void main(String[] args) {
+		System.out.println("Checking Number is odd or even using Bitwise");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number= ");
+		int num = sc.nextInt();
+		
+		if((num&1) == 1) {
+			System.out.print("Number is Odd");
+		}else {
+			System.out.print("Number is Even");
+		}
+	}
+}
