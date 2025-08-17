765e7d1f56c3561192eac55503c8a91ab58c39b9
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-17 22:19:49 +0530

    Create 22.complimentBitwise.java

---
+++ 22.complimentBitwise.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ComplimentBitwise {
+	public static void main(String[] args) {
+		System.out.println("Preforming Compliment Bitwise Operation...");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number= ");
+		int num = sc.nextInt();
+		
+		//if input is 1 then output is 0 or visaversa 
+		int result = ~num;
+		System.out.print("\nResult is "+result);	
+	}
+}
