a8d9b7fd2e0585ee1ac1c93ec180c8adad427805
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-18 08:12:53 +0530

    Create 26.parameter.java

---
+++ 26.parameter.java
@@ -0,0 +1,16 @@
+public class Parameter {
+	public static void main(String[] args) {
+		int number0 = sumTwoNum(4,8);
+		System.out.println("sum is = "+number0);
+
+		int number1 = sumTwoNum(12,8);
+		System.out.println("sum is = "+number1);
+	}
+	
+	public static int sumTwoNum(int first,int second) {
+		System.out.println("First Number is = "+first);
+		System.out.println("Second Number is = "+second);
+		int sum = first +second;
+		return sum;
+		}
+}
