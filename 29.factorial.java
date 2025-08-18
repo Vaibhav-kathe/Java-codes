c5dcf4f2ff36abf6ed181067c749da79f2536c88
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-18 08:18:00 +0530

    Create 29.factorial.java

---
+++ 29.factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Factorial {
+	public static void main(String[] args) {
+		System.out.println("Factorial of Number...");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number = ");
+		int N = sc.nextInt();
+		long factorial=1;
+		if(N<2) {
+			System.out.println("1"); 
+		}
+		for(int i=N;i>=1;i--) {
+			factorial = factorial * i;
+			System.out.print(i+" x ");
+		}
+		System.out.println("\nFactorial is = "+factorial);
+	}
+}
