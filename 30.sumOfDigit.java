813a1a4e059e2b70d31b0413faec08b38de7f4c5
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-19 23:31:55 +0530

    Create 30.sumOfDigit.java

---
+++ 30.sumOfDigit.java
@@ -0,0 +1,22 @@
+package challenges;
+import java.util.Scanner;
+public class SumOfDigit {
+	public static void main(String[] args) {
+		System.out.println("Sum of Digits .....");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Digit = ");
+		int num = sc.nextInt();
+		
+		int sum = sumofdigit(num);
+		System.out.print("Sum of digits = "+sum);
+		
+	}
+	public static int sumofdigit(int num) {
+		int sum = 0;
+		while(num>0){
+			sum =sum+num%10;
+			num = num/10;
+		}
+		return sum;
+	}
+}
