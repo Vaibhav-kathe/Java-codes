41863a7513f89bd4e555512cfa4146c6d6262f41
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-25 21:38:02 +0530

    Create 31.LCM.java

---
+++ 31.LCM.java
@@ -0,0 +1,25 @@
+package challenges;
+import java.util.Scanner;
+public class LCM {
+	public static void main(String[] args) {
+		System.out.println("Finding LCN of two Number....");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first Digit = ");
+		int first = sc.nextInt();
+		
+		System.out.print("Enter the second Digit = ");
+		int second = sc.nextInt();
+		
+		int lcm = lcm(first,second);
+		System.out.println("LCM of "+first+" and "+second+" is "+lcm);
+		sc.close();
+	}
+	public static int lcm (int first, int second) {
+		for(int i=1; i<=(first*second);i++) {
+			if((i%first)==0 && (i%second)==0) {
+				return i; //first common multiple found
+			}
+		}
+		return 0; //unreachable
+	}
+}
