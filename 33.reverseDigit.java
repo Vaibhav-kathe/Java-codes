87ac61c923cb81a2e14c5bf0cefcfe4ca6a1ddc1
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-27 21:25:30 +0530

    Create 33.reverseDigit.java

---
+++ 33.reverseDigit.java
@@ -0,0 +1,24 @@
+package challenges;
+import java.util.Scanner;
+public class ReverseDigit {
+	public static void main(String []args) {
+		System.out.println("Reversering the Digit...");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number = ");
+		int num = sc.nextInt();
+		int reverse = reverse(num);
+		System.out.println("Reversed of your number is : "+reverse);
+		
+	}
+	public static int reverse(int num) {
+		int newNum = 0;
+		while(num>0) {
+			int digit = num % 10;
+			newNum = newNum * 10 + digit;
+			num =num/10;
+		}
+		return newNum;
+
+	}
+}
