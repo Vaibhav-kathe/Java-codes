397efd0ab03122008b89a187056ce3e4ffca1ee7
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-27 21:27:27 +0530

    Create 34.fibonacciSeries.java

---
+++ 34.fibonacciSeries.java
@@ -0,0 +1,27 @@
+package challenges;
+import java.util.Scanner;
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		System.out.println("Fibonacci Series ...");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the  certain Number = ");
+		int num = sc.nextInt();
+		printfibonacci(num);
+	}
+	public static void printfibonacci(int num) {
+		if(num<=0) {
+			System.out.println("0");
+			return;
+		}
+		
+		int first = 0;
+		int second = 1;
+		while(first+second <= num) {
+			  int third = first+second;
+			  System.out.print(third +" ");
+			  first = second;
+			  second = third;
+		}	
+	}
+}
