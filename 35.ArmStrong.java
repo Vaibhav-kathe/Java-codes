84edb24348cbfd1c26f0ac986d6483feb71a19cd
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-28 20:41:42 +0530

    Update 35.ArmStrong.java

--- 35.ArmStrong.java
+++ 35.ArmStrong.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class Armstrong {
 	public static void main(String[] args) {
+		System.out.println("Checking wheather a number is ARMSTRONG is not.....");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number : ");
+		int num = sc.nextInt();
     
   }
 }
