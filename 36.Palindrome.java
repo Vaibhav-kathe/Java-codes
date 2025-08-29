a1caeb0160d2c9c419cbeb7b5e3621e909cedfc5
Vaibhav Kathe <vebzz.2230@gmail.com>
2025-08-29 12:24:24 +0530

    Update 36.Palindrome.java

--- 36.Palindrome.java
+++ 36.Palindrome.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class Palindrome {
 	public static void main(String[] args){
+		System.out.println("Checking weather a number is Palindrome or not....");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number = ");
+		int num = sc.nextInt();
 
 
 
